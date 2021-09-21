8dd8beebd86d2d7b70709b6a58cb0fcc375cf2b4
SagarMakhar <sagarmakhar@gmail.com>
2021-09-21 03:01:29 +0000

    RMX2061: Enable assert on ota zip
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: debdeep199x <debdeep199x@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@ include vendor/realme/RMX2061/BoardConfigVendor.mk
 DEVICE_PATH := device/realme/RMX2061
 
 # OTA assert
-#TARGET_OTA_ASSERT_DEVICE := RMX2061
+TARGET_OTA_ASSERT_DEVICE := RMX2061
 
 # Build
 TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
