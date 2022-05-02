a861331416bd8647b8fd56935051f3d781ca7032
debdeep199x <debdeep199x@gmail.com>
2022-05-02 04:33:23 +0000

    RMX2061: PixelExperience bring-up
    Signed-off-by: debdeep199x <debdeep199x@gmail.com>

--- aosp_RMX2061.mk
+++ aosp_RMX2061.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The DotOS
+# Copyright (C) 2018-2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -10,10 +10,8 @@ $(call inherit-product, device/realme/RMX2061/device.mk)
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-WITH_CORE_GAPPS := true
-
-# Inherit some common AospExtended stuff
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common AOSP stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier
 PRODUCT_NAME := aosp_RMX2061
@@ -22,12 +20,10 @@ PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 6 Pro
 PRODUCT_MANUFACTURER := Realme
 
-PRODUCT_GMS_CLIENTID_BASE := android-realme
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="atoll-user 11 RKQ1.200903.002/ eng.root.04080114.013112 release-keys"
-
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX2061" \
